ef10659d328bcc1015f55239b53ba60cefc6145a
Shinichi Okada <okada.shin@gmail.com>
2023-02-12 10:25:06 +0900

    Updates

---
+++ _app/immutable/chunks/1-403a6ded.js
@@ -0,0 +1,4 @@
+import { default as default2 } from "../components/error.svelte-4b2e2f16.js";
+export {
+  default2 as component
+};
---
+++ _app/immutable/chunks/2-ebc7db7b.js
@@ -0,0 +1,4 @@
+import { default as default2 } from "../components/pages/_page.svelte-308ed1c0.js";
+export {
+  default2 as component
+};
